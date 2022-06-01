a1b63484dbc9456e879638837203a0a8125caa00
linux_china <libing.chen@gmail.com>
2022-06-01 11:47:02 -0700

    refactor: rename

--- grpc-json-gateway/src/main/java/com/example/grpcserver/hello/SpringCloudGatewayJsonGrpcApplication.java
+++ grpc-json-gateway/src/main/java/com/example/grpcserver/hello/SpringCloudGatewayJsonGrpcApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringCloudGatewayGrpcApplication {
+public class SpringCloudGatewayJsonGrpcApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringCloudGatewayGrpcApplication.class, args);
+        SpringApplication.run(SpringCloudGatewayJsonGrpcApplication.class, args);
     }
 
 }
